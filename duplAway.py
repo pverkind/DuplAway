7df9f7cacfdb09da2af1cff2ec7892fe4a230455
Maxim Romanov <romanov.maxim@gmail.com>
2016-01-21 17:07:12 +0100

    it's alivegit add .git add .

--- duplAway.py
+++ duplAway.py
@@ -1,4 +1,8 @@
-import sys, re, os, difflib, fuzzywuzzy, tabulate
+# general libraries
+import sys, re, os, itertools
+
+# comparison libraries
+import difflib, fuzzywuzzy
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
@@ -21,32 +25,89 @@ def reportRatio(var1, var2):
     print("=====================")
     print("mean: %d" % mean)
 
-
-# check if duplicatedata file exists; if yes - loads it; if no - creates empty dic
+#==============================================================
+# check if duplicatedata file exists; if yes - loads it; if no - creates an empty dic
 def duplicateDataLoader(filename):
     resultsFile = filename.split(".")[0]+"_duplData.csv"
     if os.path.isfile(resultsFile):
         print("Some results already exits; incrementing...")
-        dic = {}
+        pairDic = {}
+        clusDic = {}
         with open(resultsFile, "r", encoding="utf8") as f1:
             f1 = f1.read().split("\n")
             print("\tadding to %d processed items" % len(f1))
             for line in f1:
                 line = line.split("\t")
-                dic[line[0]+"\t"+line[1]] = line[2]
+                pairDic[line[0]+"\t"+line[1]] = line[2]
+                
+                if line[2] == "y":
+                    clusDicUpdate(clusDic, [line[0], line[1]])
+        #input(clusDic)
+        clusDicSelfUpdate(clusDic)
+        updatePairDic(pairDic, clusDic)
+        # update pairDic from clusDic
     else:
         print("No results yet; creating new results variable...")
-        dic = {}
-    return(dic)
+        pairDic = {}
+        clusDic = {}
+    return(pairDic, clusDic)
 
-# saves duplicate data on exit; generates the file with approves duplicates on
+
+#==============================================================
+# list: remove duplicates and sort (for comparison)
+def fixList(l):
+    return(sorted(list(set(l))))
+
+#==============================================================
+# clustedDic updating
+def clusDicUpdate(clusDic, listVal):
+    for v in listVal:
+        if v in clusDic:
+            clusDic[v].extend(listVal)
+            clusDic[v] = fixList(clusDic[v])
+
+            for d in clusDic[v]:
+                if d in clusDic:
+                    clusDic[d].extend(clusDic[v])
+                    clusDic[d] = fixList(clusDic[d])
+                else:
+                    clusDic[d] = clusDic[v]
+        else:
+            clusDic[v] = listVal
+
+def clusDicSelfUpdate(clusDic):
+    for k,v in clusDic.items():
+        for d in v:
+            clusDic[d].extend(v)
+            clusDic[d] = fixList(clusDic[d])
+
+#==============================================================
+# if A=B and B=C, then A=C; the function does A=C
+def updatePairDic(pairDic, clusDic):
+    print("PairDic Length: %d" % len(pairDic))
+    for k,v in clusDic.items():
+        pairs = list(itertools.combinations(v, 2))
+        for p in pairs:
+            key = "\t".join(sorted(list(p)))
+            if key in pairDic:
+                pass
+            else:
+                pairDic[key] = "y"
+    print("Updated PairDic Length: %d" % len(pairDic))
+            
+  
+
+#==============================================================
+# 1. saves duplicate data on exit;
+# 2. should generate the file with approves duplicates on
 # the same line, divided with \t
-def duplicateDataSaver(filename, dic):
+def duplicateDataSaver(filename, pairDic, clusDic):
     resultsFile = filename.split(".")[0]+"_duplData.csv"
+    clusterFile = filename.split(".")[0]+"_clusData.csv"
+    
     print("Saving updated results into a file...")
     lResults = []
-    conflated = {}
-    for k,v in dic.items():
+    for k,v in pairDic.items():
         # for saving in file (loadable with duplicateDataLoader())
         lResults.append(k+"\t"+v)
             
@@ -56,48 +117,70 @@ def duplicateDataSaver(filename, dic):
     print("%d duplicate pairs saved" % len(lResults))
     print("================")
 
+    clusters = []
+    for k,v in clusDic.items():
+        val = v
+        val = fixList(val)
+        val = "\t".join(val)
+        clusters.append(val)
 
+    clusters = fixList(clusters)
+    with open(clusterFile, "w", encoding="utf8") as f9:
+        f9.write("\n".join(clusters))
+    print("================")
+    print("%d clusters saved" % len(clusters))
+    print("================")
+
+#==============================================================
 # main processing function
 def processing(filename, threshold):
-    results = duplicateDataLoader(filename)
+    os.system('clear')
+    pairDic, clusDic = duplicateDataLoader(filename)
 
     with open(filename, "r", encoding="utf8") as f1:
         testList = f1.read().split("\n")
 
         for i in testList:
             i1 = i.split("\t")[0]
-            for ii in testList:
-                ii1 = ii.split("\t")[0]
-                if i1 != ii1:
-                    testKey = "\t".join(sorted([i1, ii1]))
-                    if testKey in results:
-                        if results[testKey] == "y":
-                            pass
-                    else:
-                        # other fuzz strategies can be used
-                        if fuzz.token_set_ratio(i1, ii1) > threshold:
-                            os.system('clear') # commands clears the screen
-                            print("=====================")
-                            print(i)
-                            print("=====================")
-                            print(ii)
-                            reportRatio(i1, ii1)
-                            choice = input("Type 'y', 'n', 'm', or 'stop': ")
-                            if choice in ['y','n','m']:
-                                results[testKey] = choice
+            #print(i1)
+            if len(i1.split()) >= 2: # to avoid comparing a shorter line against a longer one
+                for ii in testList:
+                    ii1 = ii.split("\t")[0]
+                    if i1 != ii1:
+                        testKey = "\t".join(sorted([i1, ii1]))
+                        if testKey in pairDic:
+                            if pairDic[testKey] == "y":
                                 pass
-                            elif choice == "stop":
-                                break
-                            else:
-                                input("Wrong choice: %s" % choice)
-                                break
-            else:
-                continue
-            break
+                        else:
+                            # other fuzz strategies can be used
+                            if fuzz.token_set_ratio(i1, ii1) > threshold:
+                                #os.system('clear') # commands clears the screen
+                                print("=====================")
+                                print(i)
+                                print("=====================")
+                                print(ii)
+                                reportRatio(i1, ii1)
+                                choice = input("Type 'y', 'n', 'm', or 'stop': ")
+                                if choice in ['y','n','m']:
+                                    pairDic[testKey] = choice
+                                    if choice == 'y':
+                                        clusDicUpdate(clusDic, [i1, ii1])
+                                        clusDicSelfUpdate(clusDic)
+                                        updatePairDic(pairDic, clusDic)
+                                elif choice == "stop":
+                                    break
+                                else:
+                                    input("Wrong choice: %s" % choice)
+                                    break
+                                os.system('clear') # commands clears the screen
+
+                else:
+                    continue
+                break
                     
-        duplicateDataSaver(filename, results)
+        duplicateDataSaver(filename, pairDic, clusDic)
 
-
+#==============================================================
 def main():
     if len(sys.argv) != 3:
         #print(len(sys.argv))
